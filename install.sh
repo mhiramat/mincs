1803f23aa2ebd00de683ac4910990074992d925e
Masami Hiramatsu <masami.hiramatsu@gmail.com>
2015-02-22 14:14:44 +0900

    install.sh: Make sure to make destination directory

--- install.sh
+++ install.sh
@@ -8,15 +8,15 @@ LIBS="libexec/*"
 
 modify_install() { # bin target
   echo "Install $1 into $2"
+  mkdir -p `dirname $2`
   cat $1 | sed -e 's%^LIBEXEC=.*$%LIBEXEC='$LIBEXEC%g > $2
   chmod 755 $2
 }
 
-mkdir -p $BIN $LIBEXEC
-
 for i in $BINS; do
   modify_install $i $BIN/$i
 done
+
 for i in $LIBS; do
   modify_install $i $PREFIX/$i
 done
