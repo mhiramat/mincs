93f792465d0f1ae25551700feb861038c2abdbc9
Masami Hiramatsu <masami.hiramatsu@gmail.com>
2015-03-08 20:12:46 +0900

    install.sh: Support uninstall

--- install.sh
+++ install.sh
@@ -6,6 +6,23 @@ BIN=$PREFIX/bin
 BINS="minc marten polecat"
 LIBS="libexec/*"
 
+UNINSTALL=
+case "$1" in
+  -u|--uninstall)
+    UNINSTALL=yes
+    ;;
+  -h|--help)
+    echo "Install script for MINCS"
+    echo "Usage: $0 [-u|--uninstall]"
+    exit 0
+    ;;
+esac
+
+uninstall() {
+  echo "Uninstall $1 from $2"
+  rm -f $2
+}
+
 modify_install() { # bin target
   echo "Install $1 into $2"
   mkdir -p `dirname $2`
@@ -14,10 +31,18 @@ modify_install() { # bin target
 }
 
 for i in $BINS; do
-  modify_install $i $BIN/$i
+  if [ "$UNINSTALL" ]; then
+    uninstall $i $BIN/$i
+  else
+    modify_install $i $BIN/$i
+  fi
 done
 
 for i in $LIBS; do
-  modify_install $i $PREFIX/$i
+  if [ "$UNINSTALL" ]; then
+    uninstall $i $PREFIX/$i
+  else
+    modify_install $i $PREFIX/$i
+  fi
 done
 
