3fae60e88dc66fcf28efea3835c6b506033b735c
Masami Hiramatsu <masami.hiramatsu@gmail.com>
2016-10-09 13:29:39 +0900

    genkeys.sh: Add help message

--- sigpack/genkeys.sh
+++ sigpack/genkeys.sh
@@ -4,5 +4,13 @@
 # Copyright (C) 2016 Masami Hiramatsu <masami.hiramatsu@gmail.com>
 # This program is released under the MIT License, see LICENSE.
 
-openssl genrsa 4096 > $1_private.pem
-openssl rsa -in $1_private.pem -pubout -out $1_public.pem
+usage(){
+  echo "%0 - create RSA keypair"
+  echo "Usage: %0 key-pair-name"
+  exit 1
+}
+
+[ $# -ne 1 ] && usage
+
+openssl genrsa 4096 > ${1}_private.pem
+openssl rsa -in ${1}_private.pem -pubout -out ${1}_public.pem
